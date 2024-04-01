83e76c125a8b25c1d9574576525b6e016e3180e5
Brenda Mukaya <brendamukaya@gmail.com>
2024-04-01 21:31:38 +0300

    modified so as to git push

---
+++ index.js
@@ -0,0 +1,67 @@
+// Function expression called divide
+var divide = function (a,b) {
+    return a / b;
+};
+
+// Call the divide function and pass 2000 and 100 as arguments
+const Divide = divide(2000, 100);
+
+console.log('2000/100 = ', division); 
+
+// Arrow function called square
+const square = (x) => {
+    return x * x;
+};
+
+const Squared = square(2,2)
+console.log('2*2= ' ,squared)
+
+
+// Arrow function called add
+const add = (a, b) => (a + b);
+leta = 3;
+letb = 4;
+
+// Call the add function and pass a and b as arguments
+constresult = add(a, b);
+
+console.log('3+4 = ' ,result);
+
+module.exports = {
+    divide,
+    square,
+    add,
+};
+var divide = function (a,b) {
+    return a / b;
+};
+
+// Call the divide function and pass 2000 and 100 as arguments
+const division = divide(2000, 100);
+
+console.log('2000/100 = ', division); 
+
+// Arrow function called square
+constsquare= (x) => {
+    return x * x;
+};
+
+const squared = square(2,2)
+console.log('2*2= ' ,squared)
+
+
+// Arrow function called add
+const Add = (a, b) => (a + b);
+let a = 3;
+let b = 4;
+
+// Call the add function and pass a and b as arguments
+const result = add(a, b);
+
+console.log('3+4 = ' ,result);
+
+module.exports = {
+    divide,
+    square,
+    add,
+};
